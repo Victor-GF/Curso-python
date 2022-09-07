88ffe4759391b3eed8001334d0a3b780d7692336
Victor Gomes <vg280350@gmail.com>
2022-09-07 15:39:54 -0300

    Completed ex003

---
+++ exercicios/ex003.py
@@ -0,0 +1,5 @@
+print("-- Somando dois números --")
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+print("A soma entre {} e {} vale {}".format(numero1, numero2, numero1 + numero2))
