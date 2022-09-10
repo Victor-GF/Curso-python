6b4db062558888a4f80e6581541ffa7ad2875b20
Victor Gomes <vg280350@gmail.com>
2022-09-09 21:58:51 -0300

    Completed ex013

---
+++ exercicios/ex013.py
@@ -0,0 +1,7 @@
+print("-- Aplicador de Aumento --")
+
+salario = float(input("Digite seu salário atual: "))
+aumento = (15 / 100) * salario
+
+print("A partir de agora o salário irá aumentar {}$ e irá passar a ser {}$".format(aumento, salario + aumento))
+
