ada40b601fe1303a466e380a2b221521ab46863c
Victor Gomes <vg280350@gmail.com>
2022-09-09 21:36:20 -0300

    Completed ex007

---
+++ exercicios/ex007.py
@@ -0,0 +1,8 @@
+print("-- Média de Notas --")
+
+nota1 = float(input("Informe a primeira nota: "))
+nota2 = float(input("Informe a segunda nota: "))
+somaDasNotas = nota1 + nota2
+
+print("A média das notas {} e {} vale {}".format(nota1, nota2, somaDasNotas / 2))
+
