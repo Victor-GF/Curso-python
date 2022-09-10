be868b63518f3dc3074c3ddacab7bbb15ecadcd2
Victor Gomes <vg280350@gmail.com>
2022-09-09 21:38:53 -0300

    Completed ex008

---
+++ exercicios/ex008.py
@@ -0,0 +1,7 @@
+print("-- Conversor de Metro --")
+
+metros = float(input("Informe a quantidade de metros: "))
+
+print("{} metros valem {} centímetros".format(metros, metros * 100))
+print("{} metros valem {} milímetros".format(metros, metros * 1000))
+
