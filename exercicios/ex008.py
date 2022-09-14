f091e32b98c31c503a39c98000f9f9acfb8d1eb6
Victor Gomes <vg280350@gmail.com>
2022-09-13 21:46:26 -0300

    Update ex008

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -2,6 +2,23 @@ print("-- Conversor de Metro --")
 
 metros = float(input("Informe a quantidade de metros: "))
 
-print("{} metros valem {} centímetros".format(metros, metros * 100))
-print("{} metros valem {} milímetros".format(metros, metros * 1000))
+print(f"  -- {metros} Metros --")
+
+print(
+  """
+    {:.3f}  Kilômetros
+    {:.3f}   Hectômetros
+    {:.3f}    Decâmetros
+    {:.1f}   Decimetros
+    {:.1f}  Centimetros
+    {:.1f} Milimetros
+  """.format(
+    metros / 1000,
+    metros / 100,
+    metros / 10,
+    metros * 10,
+    metros * 100,
+    metros * 1000
+  )
+)
 
