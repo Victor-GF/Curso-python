274ea5967e2ee2405920984ca1bdcfdefe6e0297
Victor Gomes <vg280350@gmail.com>
2022-09-13 22:02:31 -0300

    Update ex011

--- exercicios/ex011.py
+++ exercicios/ex011.py
@@ -1,9 +1,9 @@
 print("-- Rendimento de Tinta --")
 
-largura = float(input("Informe a largura da parede: "))
-altura = float(input("Informe a altura da parede: "))
+largura = float(input("Informe a largura da parede em metros: "))
+altura = float(input("Informe a altura da parede em metros: "))
 area = largura * altura
 
 litrosDeTinta = area / 2
-print("Para uma área de {} metros² serão necessários {} litros de tinta".format(area, litrosDeTinta))
+print("Para uma área de {}m² serão necessários {:.1f} litros de tinta".format(area, litrosDeTinta))
 
