3242cd9e2d4df7cbe0782d9a66a2f2bdadb19a88
Victor Gomes <vg280350@gmail.com>
2022-09-13 21:58:46 -0300

    Update ex010

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,9 +1,9 @@
 print("-- Contador de Dólares --")
 
-reais = float(input("Informe quantos reais você tem: "))
-valorDoDolar = float(input("Informe o valor atual do dólar: "))
+reais = float(input("Informe quantos reais você tem: R$"))
+valorDoDolar = float(input("Informe o valor atual do dólar: U$"))
 
-dolares = int(reais // valorDoDolar)
+dolares = reais / valorDoDolar
 
-print("{} reais conseguem comprar {} dólares".format(reais, dolares))
+print("R${:.2f} reais conseguem comprar U${:.2f} dólares".format(reais, dolares))
 
