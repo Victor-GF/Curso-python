2da61e3bb3f93628050c726f76dbe127dbe22778
Victor Gomes <vg280350@gmail.com>
2022-09-15 21:49:19 -0300

    Update ex014

--- exercicios/ex014.py
+++ exercicios/ex014.py
@@ -4,7 +4,7 @@ from xml.dom.expatbuilder import FragmentBuilder
 print("--    Conversor de Temperatura    --")
 
 celsius  = float(input("Informe a temperatura em ºC: "))
-fahrenheit = (celsius * 9/5) + 32
+fahrenheit = celsius * 9/5 + 32
 
 print("{:.2f}ºC correspondem a {:.2f}F".format(celsius, fahrenheit))
 
