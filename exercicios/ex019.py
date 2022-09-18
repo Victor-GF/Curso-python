e6fa8de88dd8c4b0b8866ead055dbb2cc50a0e40
Victor Gomes <vg280350@gmail.com>
2022-09-18 15:10:37 -0300

    Update ex019

--- exercicios/ex019.py
+++ exercicios/ex019.py
@@ -5,5 +5,5 @@ aluno2 = input("->  Aluno 2: ")
 aluno3 = input("->  Aluno 3: ")
 aluno4 = input("->  Aluno 4: ")
 
-print("O aluno escolhido foi: {}".format(choice((aluno1, aluno2, aluno3, aluno4))))
+print(">> O aluno escolhido foi: {}".format(choice((aluno1, aluno2, aluno3, aluno4))))
 
