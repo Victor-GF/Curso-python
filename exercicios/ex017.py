05483420d20374bbeec0b8b2de2e1058a005504c
Victor Gomes <vg280350@gmail.com>
2022-09-18 14:55:10 -0300

    Update ex017

--- exercicios/ex017.py
+++ exercicios/ex017.py
@@ -3,9 +3,9 @@ from math import hypot
 
 print(emojize("--  Calculadora de Hipotenusa :cat_face:  --"))
 
-catOposto = float(input("Informe o valor do cateto oposto: "))
-catAdjacente = float(input("Informe o valor do cateto adjacente: "))
+catOposto = float(input("-> Informe o valor do cateto oposto: "))
+catAdjacente = float(input("-> Informe o valor do cateto adjacente: "))
 hipotenusa = hypot(catOposto, catAdjacente)
 
-print("O valor da Hipotenusa é: {:.2f}".format(hipotenusa))
+print(">> O valor da Hipotenusa é: {:.2f}".format(hipotenusa))
 
