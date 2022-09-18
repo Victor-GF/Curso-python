fd3593557ad9bb821d49d9f514a65b8dd8c69ded
Victor Gomes <vg280350@gmail.com>
2022-09-18 15:03:41 -0300

    Update ex018

--- exercicios/ex018.py
+++ exercicios/ex018.py
@@ -1,10 +1,10 @@
 from math import tan, sin, cos, radians
 
-angulo = radians(float(input("Informe o valor do ângulo em graus: ")))
+angulo = radians(float(input("-> Informe o valor do ângulo em graus: ")))
 
 print(
-  """  O seno é: {:.2f}
-  O cosseno é: {:.2f}
-  A tangente é: {:.2f}""".format(sin(angulo), cos(angulo), tan(angulo))
+  """>> O SENO é: {:.2f}
+>> O COSSENO é: {:.2f}
+>> A TANGENTE é: {:.2f}""".format(sin(angulo), cos(angulo), tan(angulo))
 )
 
